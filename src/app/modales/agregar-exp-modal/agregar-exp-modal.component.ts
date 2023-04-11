7fd178eee1e6bf36404a01f24008a0db8ab651ab
Alejandra C Romero <aleceromero@gmail.com>
2023-04-11 18:11:39 -0300

    conectar back-front Angular modal agregar experiencia

--- src/app/modales/agregar-exp-modal/agregar-exp-modal.component.ts
+++ src/app/modales/agregar-exp-modal/agregar-exp-modal.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Experiencia } from 'src/app/model/experiencia';
+import { ExperienciaService } from 'src/app/servicios/experiencia.service';
 
 @Component({
   selector: 'app-agregar-exp-modal',
@@ -10,13 +12,20 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class AgregarExpModalComponent implements OnInit {
 
   form: FormGroup;
+  puesto: string = '';
+  fechaInicio: string ='';
+  fechaFin: string='';
+  empresa: string='';
+  descripcion: string='';
 
-  constructor(private formBuilder: FormBuilder) { 
+  constructor(private formBuilder: FormBuilder, private expeServ: ExperienciaService) { 
     // grupo de controles formulario
     this.form = this.formBuilder.group({
       addPuesto:['', [Validators.required]],
       addFechaInicio:['', [Validators.required]],
+      addFechaFin:[''],
       addEmpresa:['', [Validators.required]],
+      addDescripcion:[''],
     })
   }
 
@@ -32,8 +41,16 @@ export class AgregarExpModalComponent implements OnInit {
     return this.form.get("addFechaInicio");
   }
 
+  get FechaFin(){
+    return this.form.get("addFechaFin");
+  }
+
   get Empresa(){
     return this.form.get("addEmpresa");
+  }
+
+  get Descripcion(){
+    return this.form.get("addDescripcion");
   }
 
   limpiar(): void{
@@ -46,12 +63,18 @@ export class AgregarExpModalComponent implements OnInit {
 
     if (this.form.valid){
     // Llamamos a nuestro servicio para enviar los datos al servidor
-      alert("Todo salio bien ¡Enviar formuario!")
+      this.onCreate();
     } else {
-    // Corremos todas las validaciones para que se ejecuten los mensajes de error en el template     
+    // Corremos todas las validaciones para que se ejecuten los mensajes de error en el template  
+      alert("Falló la carga, intente nuevamente");
       this.form.markAllAsTouched(); 
     }
   }
 
+  onCreate(): void {
+    const expe = new Experiencia(this.puesto, this.fechaInicio, this.fechaFin, this.empresa, this.descripcion);
+      this.expeServ.saveExperiencia(expe).subscribe(data => {alert("Experiencia añadida")});
+      window.location.reload();
+  }
 
 }
